fb297c5245611b70873722b3f5dd1bf4f11336a1
bigyxxn <bigyxxn@gmail.com>
2024-06-10 21:13:44 +0900

    Initial commit

---
+++ src/test/java/com/bigyxxn/payment/eureka/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigyxxn.payment.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
